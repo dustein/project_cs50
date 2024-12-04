1e52ab9973b6104570d7f06fe8f233a389e09a5b
dustein <steindu@gmail.com>
2024-12-04 10:50:24 -0300

    retomando

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, session, redirect, request
 from flask_session import Session
 from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import error_msg
 
 app = Flask(__name__)
 
@@ -17,16 +19,38 @@ Session(app)
 @app.route("/")
 def hello_world():
     print("Hello World")
-    message = "teste"
+    message = "pAGINA iNICIAL INDEX..."
     return render_template("error_msg.html", message=message)
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    #limpar dados de sessao anterior
+    session.clear()
 
     if request.method == "POST":
+
         username = form.request.get("username")
+        if not username:
+            return error_msg("Usuário não informado.", 403)
         password = form.request.get("password")
+        if not password:
+            return error_msg("Senha não informada.", 403)
+        re_password = form.request.get("re_password")
+        if not re_password:
+            return error_msg("Repita a Senha para confirmar.", 403)
+        if (password != re_password):
+            return error_msg("Senhas informadas divergem entre si.", 403)
+
+        # verificar se o username já existe no banco de dados
+        # confirmar se a senha confere com a senha repetida
+
+        # gerar o hash pra senha informada
+        hash_password = generate_password_hash(password, method='scrypt', salt_length=16)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", "Edu", hash_password)
+
 
         return redirect("/")
+
     else:
         return render_template("login.html")
